3468f7e4ca6dba8e074b36d2a9d6baf5fa6bb404
Xerexos <ceddy9176@gmail.com>
2025-06-20 08:25:12 -0700

    added Bismuth Block & Bismuth Ore

--- src/main/java/com/xerexos/practicemod/PracticeMod.java
+++ src/main/java/com/xerexos/practicemod/PracticeMod.java
@@ -1,6 +1,7 @@
 package com.xerexos.practicemod;
 
-import com.xerexos.practicemod.item.Moditems;
+import com.xerexos.practicemod.block.ModBlocks;
+import com.xerexos.practicemod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
@@ -18,6 +19,8 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+import static com.xerexos.practicemod.block.ModBlocks.*;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(PracticeMod.MOD_ID)
@@ -38,7 +41,8 @@ public class PracticeMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
-        Moditems.register(modEventBus);
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -54,8 +58,15 @@ public class PracticeMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(Moditems.BISMUTH);
-            event.accept(Moditems.RAW_BISMUTH);
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
+
 
         }
     }
