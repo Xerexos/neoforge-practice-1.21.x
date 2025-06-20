b097016269505a6538079e29be44530b8fc8253d
Xerexos <ceddy9176@gmail.com>
2025-06-20 09:29:13 -0700

    added Creative mode Tabs for Bismuth items & Bismuth Blocks

--- src/main/java/com/xerexos/practicemod/PracticeMod.java
+++ src/main/java/com/xerexos/practicemod/PracticeMod.java
@@ -34,7 +34,7 @@ public class PracticeMod {
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public PracticeMod(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
+        // Register the commonSetup method for modloading 
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in.
